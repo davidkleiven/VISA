8557da8d97d3d58ded0568a7f86b126a6ee554e7
David Kleiven <davidkleiven446@gmail.com>
2016-12-10 12:06:04 +0000

    Added doxygen comments

--- include/windowHandler.hpp
+++ include/windowHandler.hpp
@@ -8,14 +8,26 @@ class WindowHandler
 public:
   WindowHandler(){};
   ~WindowHandler();
+
+  /** Add a plot that will be displayed in a separate window */
   void addPlot( const char* name );
+
+  /** Get the visualization object */
   Visualizer& get( const char* name );
+
+  /** Get active visualization object */
   Visualizer& getActive();
+
+  /** Set active visualization window */
   void setActive( const char* name );
+
+  /** Show all plots */
   void show();
 private:
   std::vector<Visualizer*> plots;
   Visualizer* active{NULL};
+
+  /** If the active window is closed, this sets a new active window */
   void resetActive();
 };
 #endif
