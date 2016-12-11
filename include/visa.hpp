48e9ebaec31fa751cbf2cc31e1932e77372585c5
David Kleiven <davidkleiven446@gmail.com>
2016-12-11 10:09:43 +0000

    A single header file for all the headers in the project

---
+++ include/visa.hpp
@@ -0,0 +1,12 @@
+#ifndef VISA_H
+#define VISA_H
+
+#include "colormaps.hpp"
+#include "fifoBuffer.hpp"
+#include "gaussianKernel.hpp"
+#include "lowPassFilter.hpp"
+#include "visualizer.hpp"
+#include "visualizer1D.hpp"
+#include "windowHandler.hpp"
+
+#endif
